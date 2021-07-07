fd5749b457c6596090972e83f20f1132d92e139c
khadim sarr <ksarr@gainde2000.sn>
2021-07-07 08:49:31 +0000

    first commit

---
+++ src/main/java/com/devglan/WebsocketBakendApplication.java
@@ -0,0 +1,13 @@
+package com.devglan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketBakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketBakendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devglan/config/WebSocketConfig.java
@@ -0,0 +1,36 @@
+package com.devglan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig  extends AbstractWebSocketMessageBrokerConfigurer {
+
+	
+	
+	@Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket")
+                //.setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/message");
+    }
+    
+    
+    
+	
+	
+}
